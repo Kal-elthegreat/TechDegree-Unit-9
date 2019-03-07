eadd259bbf5b43a006d6644cba9251846da0d898
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-03-07 09:29:21 -0500

    Configured Mongoose

---
+++ mongoose.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', function (err){
+    console.error('Connection error:', err);
+});
+
+db.once('open', function (){
+    console.log('db connection successful')
+    // all db communcication goes here
+    var Schema = mongoose.Schema
+    var UserSchema = new Schema ({
+        firstName: String,
+        lastName: String,
+        emailAddress: String,
+        password: String
+    });
+    var User = mongoose.model('User', UserSchema);
+    
+    var CourseSchema = new Schema ({
+        user: UserSchema,
+        title: String,
+        description: String,
+        estimatedTime: String,
+        materialsNeeded: String
+    });
+    var Course = mongoose.model('Course', CourseSchema);
+
+
+    db.close();
+})
+
+
+
