77d3caef10cff08a16c65aa9a156153a2213e222
Kiel Lewis <kiellewis@Kiels-MBP.hsd1.ga.comcast.net>
2019-03-12 23:18:57 -0400

    Redid routes.js
    authUser middleware needs work. need to query my User db.
    
    GET - courses, courses:id, users(no auth) - all work.
    POST - user, course - both work (user._id needs to be passed to POST course).
    PUT/DELETE course - both work.

--- routes.js
+++ routes.js
@@ -7,7 +7,43 @@ const User = require('./models').User;
 const bcryptjs = require('bcryptjs');
 const auth = require('basic-auth');
 
+//const users = [{emailAddress: 'paul@wall.com', password: '$2a$10$aZJon4If7ZskfteR.O3wWe0U.xW4I.k5ePsYSnzaB2b.rvSRYvfrC'}] // ignore this
 
+const authUser = (req,res,next) => {
+    let message = null;
+    const credentials = auth(req);
+    if(credentials.name && credentials.pass){
+        console.log('good: ' + credentials);
+        //const user = users.find(u => u.emailAddress === credentials.name);
+       const user = User.findOne({emailAddress: credentials.name})
+       .exec(function(err,users){
+        // ?? what goes here ??
+       })
+        if(user){
+            console.log('user avail: ' + user.password, credentials)
+            const auth = bcryptjs
+            .compareSync(credentials.pass, user.password);
+            console.log(auth)
+            if(auth){
+                console.log('User authenticated')
+                req.currentUser = user;
+            } else{
+                message = 'Authentication fail';
+                //onsole('User not authenticated')
+            }
+        } else {
+            message = 'User not found';
+            //console.log('no user here' + user)
+        }
+    } else {
+        message = 'Username/Password';
+        console.log( 'Username or Password needed', credentials.name + credentials.pass)
+    }
+
+    if(message){
+        console.warn(message);
+    }
+};
 
 router.param("id", function(req, res,next,id){
     Course.findById(id, function(err,doc){
@@ -23,60 +59,36 @@ router.param("id", function(req, res,next,id){
     .populate('user')
 });
 
-
-const authenticateUser = (req, res, next) => {
-    // to do
-
-    const credentials = auth(req);
-
-    if(credentials){
-        console.log(1) // test 1 
-        User.findOne({emailAddress: credentials.name}) // <--- issue lies here, cannot access User db for some reason
-        .exec(function(error, user){
-            console.log(user)
-            if(error) {
-                console.log('error here')
-                next(error);
-            } 
-            if(user){
-                console.log(2, 'user: ' + user, 'credentials: ' + credentials.pass) // user holds- obj obj
-                const auth = bcryptjs
-                .compareSync(credentials.pass, user.password);
-                if (auth){
-                    console.log(3) // test 3
-                    req.currentUser = user;
-                }
-            }    
-        })
-    
-    }
-    next();
-
-};
     
 
 // GET USERS - working
-router.get('/users', authenticateUser, (req,res) => {
-    const user = req.currentUser;
-    //console.log('test val: ' + user)
-    res.json({ // -- keep failing here, user is returning undefined 
-    users: user,
-    //   username: user.emailAddress,
-    //   password: user.password
-    });
+router.get('/users', (req,res, next) => {
+    /// this block will be replaced once authUser works
+    User.find({})
+    .exec(function(err,users){
+        if(err) return next(err);
+        res.json(users)})
+    /////
+
+    //const user = req.currentUser;
+    //res.json({ // -- keep failing here, user is returning undefined 
+    //     users: user,
+    //     username: user.emailAddress,
+    //    password: user.password
+    //});
 });
 
 // POST USERS - working validation complete
 router.post('/users',(req,res,next) => {
     const user = new User(req.body);
-
+    
+    if(user.password){
     // hash new user password
-   user.password = bcryptjs.hashSync(user.password);
-
-    user.save(function(err, user){
+    user.password = bcryptjs.hashSync(user.password);
+    };
+    
+    user.save(function(err){
         if(err) return res.status(400).json({error: err.message});
-
-        users.push(user); // -- possible problem
          // set status to 201 created
         res.sendStatus(201);
         
@@ -99,18 +111,18 @@ router.get('/courses/:id', (req,res,next) => {
 });
 
 // POST COURSES - working and valid
-router.post('/courses', authenticateUser, (req,res) => {
+router.post('/courses',  (req,res,next) => {
 
     const newCourse = new Course({
-        title: req.title,
-        description: req.description,
-        estimatedTime: req.estimatedTime,
-        materialsNeeded: req.materialsNeeded,
-        user: user._id
+        title: req.body.title,
+        description: req.body.description,
+        estimatedTime: req.body.estimatedTime,
+        materialsNeeded: req.body.materialsNeeded
+        //user: user._id
     });
 
     const course = new Course(newCourse);
-    course.save(function(err, course){
+    course.save(function(err){
         if(err) return next(err);
         else {
             res.sendStatus(201)
@@ -120,7 +132,7 @@ router.post('/courses', authenticateUser, (req,res) => {
 });
 
 // PUT COURSES:id - working and valid 
-router.put('/courses/:id', authenticateUser, (req,res) => {
+router.put('/courses/:id', (req,res) => {
     req.course.update(req.body, function(err){
         if(err) return res.status(400).json({errors: err.message});
         res.sendStatus(204);
@@ -128,7 +140,7 @@ router.put('/courses/:id', authenticateUser, (req,res) => {
 });
 
 // DELETE COURSES - working
-router.delete('/courses/:id', authenticateUser, (req,res) => {
+router.delete('/courses/:id',(req,res) => {
     req.course.remove(function(err){
         req.course.save(function(err,course){
             if (err) return next(err);
@@ -138,32 +150,3 @@ router.delete('/courses/:id', authenticateUser, (req,res) => {
 });
 
 module.exports = router;
-
-// if(credentials){
-//     user = User.findOne({emailAddress : credentials.name}, function (err,user){
-//         console.log('chose user: ' + user + 'cred' + credentials)
-//         if(user){
-//             //console.log('chose user: ' + user + 'cred' + credentials)
-//             const auth = bcryptjs.compareSync(user.password, credentials.pass);
-//             if(auth){ 
-//                 console.log(`Authentication successful for username: ${user.emailAddress}`);
-//                 req.currentUser = user;
-//                 console.log('currentUser val: ' + req.currentUser)
-//             } else {
-//                 message = `Authentication failure for username: ${user.emailAddress}`;
-//             }
-//         } else {
-//             message = `Authentication failure for username: ${credentials.name}`
-//         }
-//     });
-// } else {
-// message = 'Auth header not found';
-// };
-// // this portion is not working correctly message is returning null everytime
-// if(message) {
-//     console.warn(message);
-//     res.status(401).json({message: 'Access Denied'});
-// } else {
-//     next();
-// }
-// };
